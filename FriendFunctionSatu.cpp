c5b24c301f8c1acfbd4ef3da9860d47737493c42
pichiefox <citrazad@gmail.com>
2026-04-28 15:40:01 +0700

    Class Mahasiswa

--- FriendFunctionSatu.cpp
+++ FriendFunctionSatu.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
 #include <string>
 using namespace std;
+class mahasiswa
+{
+    private:
+        string nama;
+    public:
+        friend void setNama(mahasiswa &a, string b);
+}
