3944dc292d8d2efc8d65a7b23bf910a15b819501
pichiefox <citrazad@gmail.com>
2026-04-28 15:05:05 +0700

    Pointer Reference refMhs

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -12,7 +12,7 @@ int main (){
     mahasiswa mhs{1}; //Object mhs
     mhs.showNim(); //Member Access Operator
 
-    mahasiswa refMhs = mhs;
+    mahasiswa refMhs = mhs; //Pointer Reference refMhs
     refMhs.nim = 2;
     mhs.showNim();
 
